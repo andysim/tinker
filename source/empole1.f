08295c49f49ce2e12d9ac85f085c0c789328a3a8
Andy Simmonett <andy.simmonett@gmail.com>
2015-09-17 12:07:48 -0600

    Avoided repeated exp calls in rec space virial for OPT.

--- source/empole1.f
+++ source/empole1.f
@@ -6391,6 +6391,7 @@ c
       integer i,j,k,ii
 c OPT IMPLEMENTATION
       integer o,l,m
+      real*8, allocatable :: strc2(:,:,:)
 c OPT IMPLEMENTATION
       integer j1,j2,j3
       integer k1,k2,k3
@@ -6489,6 +6490,9 @@ c
 c     perform dynamic allocation of some local arrays
 c
       allocate (qgrip(2,nfft1,nfft2,nfft3))
+c OPT IMPLEMENTATION
+      allocate (strc2(nfft1,nfft2,nfft3))
+c OPT IMPLEMENTATION
 c
 c     transform permanent multipoles without induced dipoles
 c
@@ -7092,18 +7096,31 @@ c     perform deallocation of some local arrays
 c
 
 c
-c        add in the PT mutual terms
+c     add in the PT mutual terms
 c
+      if(poltyp(1:3) .eq. 'OPT') then
+         strc2 = 0d0
          do l = 0, ptmaxord-1
             do m = 0, ptmaxord-1
                if(abs(ptcoefs(l+m+1)) .lt. 1D-6) cycle
-               call scalarsum(uindgridf(:,:,:,:,l), uinpgridf(:,:,:,:,m)
-     &               ,ptcoefs(l+m+1),vxx,vyx,vzx,vyy,vzy,vzz)
+                do k = 1, nfft3
+                   do j = 1, nfft2
+                      do i = 1, nfft1
+                         strc2(i,j,k) = strc2(i,j,k) + ptcoefs(l+m+1)*
+     &                     ( uindgridf(1,i,j,k,l)*uinpgridf(1,i,j,k,m)
+     &                     + uindgridf(2,i,j,k,l)*uinpgridf(2,i,j,k,m) )
+                      end do
+                   end do
+                end do
             enddo
          enddo
+         call ssum(strc2, 1.0d0, vxx, vyx, vzx, vyy, vzy, vzz)
+      endif
+
+      deallocate (strc2)
 c OPT IMPLEMENTATION
       deallocate (qgrip)
-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+
 c
 c     increment the internal virial tensor components
 c
@@ -7205,6 +7222,95 @@ c
             end if
             struc2 = gridd(1,k1,k2,k3)*gridp(1,k1,k2,k3)
      &                  + gridd(2,k1,k2,k3)*gridp(2,k1,k2,k3)
+            eterm = 0.5d0 * electric * expterm * struc2
+            vterm = (2.0d0/hsq) * (1.0d0-term) * eterm
+            myvxx = myvxx + h1*h1*vterm - eterm
+            myvyx = myvyx + h2*h1*vterm
+            myvzx = myvzx + h3*h1*vterm
+            myvyy = myvyy + h2*h2*vterm - eterm
+            myvzy = myvzy + h3*h2*vterm
+            myvzz = myvzz + h3*h3*vterm - eterm
+         end if
+         qfac(k1,k2,k3) = expterm
+      end do
+      vxx = vxx + scalefac*myvxx 
+      vyx = vyx + scalefac*myvyx 
+      vzx = vzx + scalefac*myvzx 
+      vyy = vyy + scalefac*myvyy 
+      vzy = vzy + scalefac*myvzy 
+      vzz = vzz + scalefac*myvzz 
+      end
+
+      subroutine ssum(str2,scalefac,vxx,vyx,vzx,vyy,vzy,vzz)
+      use pme
+      use chgpot
+      use ewald
+      use boxes
+      use bound
+      use math
+      implicit none
+      integer ntot,nff
+      real*8 scalefac
+      real*8 vxx,vyx,vzx
+      real*8 vyy,vzy,vzz
+      real*8 myvxx,myvyx,myvzx
+      real*8 myvyy,myvzy,myvzz
+      real*8 volterm,denom
+      real*8 hsq,expterm
+      real*8 term,pterm
+      real*8 vterm,struc2,eterm
+      real*8 r1,r2,r3
+      real*8 h1,h2,h3
+      real*8 str2(nfft1,nfft2,nfft3)
+      integer k1,k2,k3
+      integer m1,m2,m3
+      integer nf1,nf2,nf3
+      integer i,j,k
+c
+c     make the scalar summation over reciprocal lattice
+c
+      ntot = nfft1 * nfft2 * nfft3
+      pterm = (pi/aewald)**2
+      volterm = pi * volbox
+      nff = nfft1 * nfft2
+      nf1 = (nfft1+1) / 2
+      nf2 = (nfft2+1) / 2
+      nf3 = (nfft3+1) / 2
+      myvxx = 0d0
+      myvyx = 0d0
+      myvzx = 0d0
+      myvyy = 0d0
+      myvzy = 0d0
+      myvzz = 0d0
+      do i = 1, ntot-1
+         k3 = i/nff + 1
+         j = i - (k3-1)*nff
+         k2 = j/nfft1 + 1
+         k1 = j - (k2-1)*nfft1 + 1
+         m1 = k1 - 1
+         m2 = k2 - 1
+         m3 = k3 - 1
+         if (k1 .gt. nf1)  m1 = m1 - nfft1
+         if (k2 .gt. nf2)  m2 = m2 - nfft2
+         if (k3 .gt. nf3)  m3 = m3 - nfft3
+         r1 = dble(m1)
+         r2 = dble(m2)
+         r3 = dble(m3)
+         h1 = recip(1,1)*r1 + recip(1,2)*r2 + recip(1,3)*r3
+         h2 = recip(2,1)*r1 + recip(2,2)*r2 + recip(2,3)*r3
+         h3 = recip(3,1)*r1 + recip(3,2)*r2 + recip(3,3)*r3
+         hsq = h1*h1 + h2*h2 + h3*h3
+         term = -pterm * hsq
+         expterm = 0.0d0
+         if (term .gt. -50.0d0) then
+            denom = volterm*hsq*bsmod1(k1)*bsmod2(k2)*bsmod3(k3)
+            expterm = exp(term) / denom
+            if (.not. use_bounds) then
+               expterm = expterm * (1.0d0-cos(pi*xbox*sqrt(hsq)))
+            else if (octahedron) then
+               if (mod(m1+m2+m3,2) .ne. 0)  expterm = 0.0d0
+            end if
+            struc2 = str2(k1,k2,k3)
             eterm = 0.5d0 * electric * expterm * struc2
             vterm = (2.0d0/hsq) * (1.0d0-term) * eterm
             myvxx = myvxx + h1*h1*vterm - eterm
